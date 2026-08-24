d0376bbc6b61423c86ff40afd5b430301bb2d940
santiagodev <sanxp18@gmail.com>
2026-08-24 15:06:53 -0400

    feat: implement setup for tests of body mass index calculation
    - Add js class Person with method to calculate body mass index and its respective test file.
    - Add two scripts to the package.json to run eslint for a single file, to find syntax errors and to fix them.
    - Add setup to the 02-math.test.js file.

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,24 +1,34 @@
 const { sum, multiply, divide, mean } = require("./02-math");
 
-test("adds 1 + 2 to equal 3", () => {
-	const answer = sum(1, 2);
-	expect(answer).toBe(3);
-});
+describe("Tests for math operations", () => {
+	describe("Test for sum", () => {
+		test("adds 1 + 2 to equal 3", () => {
+			const answer = sum(1, 2);
+			expect(answer).toBe(3);
+		});
+	});
 
-test("multiplies 2 * 3 to equal 6", () => {
-	expect(multiply(2, 3)).toBe(6);
-});
+	describe("Test for multiplication", () => {
+		test("multiplies 2 * 3 to equal 6", () => {
+			expect(multiply(2, 3)).toBe(6);
+		});
+	});
 
-test("should divide", () => {
-	const answer1 = divide(6, 2);
-	const answer2 = divide(6, 3);
-	const answer3 = divide(6, 0);
+	describe("Test for division", () => {
+		test("should divide", () => {
+			const answer1 = divide(6, 2);
+			const answer2 = divide(6, 3);
+			const answer3 = divide(6, 0);
 
-	expect(answer1).toBe(3);
-	expect(answer2).toBe(2);
-	expect(answer3).toBe(null);
-});
+			expect(answer1).toBe(3);
+			expect(answer2).toBe(2);
+			expect(answer3).toBe(null);
+		});
+	});
 
-test("should calculate mean", () => {
-	expect(mean(10, 20, 30)).toBe(20);
+	describe("Test for mean", () => {
+		test("should calculate mean", () => {
+			expect(mean(10, 20, 30)).toBe(20);
+		});
+	});
 });
---
+++ demos/src/06-person.test.js
@@ -0,0 +1,23 @@
+const Person = require("./06-person");
+
+describe("Test for Person", () => {
+	let person;
+
+	beforeEach(() => {
+		person = new Person("Santiago", 58, 1.7);
+	});
+
+	test("should return down", () => {
+		person.weight = 45;
+		const imc = person.calcIMC();
+
+		expect(imc).toBe("down");
+	});
+
+	test("should return normal", () => {
+		person.weight = 59;
+		const imc = person.calcIMC();
+
+		expect(imc).toBe("normal");
+	});
+});
